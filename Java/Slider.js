42fa24439adb0297c7570295f203a15030a8e074
K3vin030 <k3vin02003@outlook.com>
2022-05-31 16:54:58 -0500

    Landdrrr2

--- Java/Slider.js
+++ Java/Slider.js
@@ -8,35 +8,32 @@
 
 	$('.Slider li').hide();
 	$('.Slider li:first').show();
+	
+
 	$('.Right span').click(nextSlider);
 	$('.Left span').click(prevSlider);
+
+
+	setInterval(function(){
+		nextSlider();
+	}, 7000);
 
 
 
 	//FUNCIONES ========================================================================
 
 	function nextSlider(){
-		if (imgPos >=imgItems) {
-				imgPos = 1
-		} else {
-			imgPos++;
-		}
+		if (imgPos >=imgItems) {imgPos = 1} 
+		else {imgPos++;}
 		
 		$('.Slider li').hide();
-		$('.Slider li:nth-child('+ imgPos + ')').fadeIn();
+		$('.Slider li:nth-child(' + imgPos + ')').fadeIn();
 	}
 
 	function prevSlider(){
-		if (imgPos <= 1) {
-				imgPos = imgItems
-		} else {
-			imgPos++;
-		}
+		if (imgPos <= 1) {imgPos = imgItems;} 
+		else {imgPos--;}
 		
 		$('.Slider li').hide();
 		$('.Slider li:nth-child('+ imgPos + ')').fadeIn();
 	}
-
-	setInterva(function(){
-		nextSlider();
-	}, 4000);
